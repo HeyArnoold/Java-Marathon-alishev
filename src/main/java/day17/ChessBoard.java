b507c735f1b9af2119e1fbc9c70b674812189804
ÐMaksim <maks.antonov.2012@gmail.com>
2021-12-13 22:22:26 +0300

    complete day17 tasks

---
+++ src/main/java/day17/ChessBoard.java
@@ -0,0 +1,20 @@
+package day17;
+
+public class ChessBoard {
+    private ChessPiece [][] chessBoard;
+
+    public ChessBoard(ChessPiece[][] chessBoard) {
+        this.chessBoard = chessBoard;
+    }
+
+    public void print() {
+        for (int i = 0; i < chessBoard.length; i++) {
+
+            for (int j = 0; j < chessBoard[i].length; j++) {
+
+                System.out.print(chessBoard[i][j].getName());
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,13 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece white = ChessPiece.PAWN_WHITE;
+        ChessPiece black = ChessPiece.ROOK_BLACK;
+        String[] chessPiece = {white.getName(), white.getName(), white.getName(), white.getName(),
+                                black.getName(), black.getName(), black.getName(), black.getName()};
 
+        for (String name : chessPiece) {
+            System.out.print(name + " ");
+        }
     }
 }
